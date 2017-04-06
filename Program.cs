03c34613e8125762f2b37a59ff2b6b69988fd7f8
Bohdan <x.bodya6@gmail.com>
2017-04-06 07:55:50 +0300

    new_1

--- Program.cs
+++ Program.cs
@@ -10,9 +10,29 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("lol1");
+            Point p1 = new Point(1, 3, '@');
+            p1.Draw();
+            
             Console.ReadLine();
 
+            List< int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+           foreach(int i in numList)
+            {
+                Console.Write(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
         }
     }
 }
